41e5f2562b448c29f7758b40e7f010a86d4a07a6
Dandav2923 <daniele.emili23@gmail.com>
2021-11-29 15:19:47 +0100

    soluzione senza funzione

--- js/script.js
+++ js/script.js
@@ -4,11 +4,24 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 // (prima senza funzione e poi con funzione)
 // Chiediamo all'utente una parola 
-const wordUser = prompt('Inswerisci una parola');
+let wordUser = prompt('Inswerisci una parola');
 // dividiamo la parole per singole stringhe così da poterle confrontare con la parola inserita dall'utente 
-const wordSplit = wordUser.split('');
+let wordSplit = wordUser.split('');
 console.log(wordSplit);
 // invertiamo le singole stringe in una variabile da poter utilizzare nel confronto con la parola inserita dall'utente 
-const wordSplit = wordSplit.reverse;
+let wordReverse = wordSplit.reverse();
 console.log(wordSplit);
 
+// se effettuassimo la condizione ora tra le variabili non riusciremo ad andare avanti poichè abbiamo diviso ed invertito le stringhe, quindi esiste un modo per ricongiungere le stringhe? 
+// L'internet ha la risposta sicuramente.
+
+let wordJoin= wordSplit.join('');
+console.log(wordReverse);
+
+// ora abbiamo la parola ricostruita e possiamo impostare la condizione 
+
+if (wordUser == wordJoin) {
+    console.log('La parola inserita è palindroma');
+} else {
+    console.log('La parola inserita non è palindroma');
+}
